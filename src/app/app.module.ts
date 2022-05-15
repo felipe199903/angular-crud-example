07c95b92d2945ba96fde8ed39df439ab778b1351
Felipe Schorles <felipeschorles@MacBook-Pro-de-Felipe.local>
2022-05-15 13:49:57 -0300

    bug/env-url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { FooterComponent } from './core/footer/footer.component';
 import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
     MainComponent
   ],
   imports: [
